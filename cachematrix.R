4d124c26630601c34a62e5bcc83017a1ba55a8ec
Santiago Mota <santiago_mota@yahoo.es>
2014-04-23 15:09:40 +0200

    First working version
    I include the two functions and two examples of use

--- cachematrix.R
+++ cachematrix.R
@@ -14,18 +14,56 @@
 
 ## For this assignment, assume that the matrix supplied is always invertible.
 
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## In order to make this function, we use the makeVector function of the example,
+## but changing the mean function to solve. As in the example, the function
+## solve the inverse of the matrix and store it to use later in cacheSolve 
+## function
 
 makeCacheMatrix <- function(x = matrix()) {
-      
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse) 
 }
 
 
-## Write a short comment describing this function
+## In order to make this function, we use the cachemean function of the example,
+## but changing the mean function to solve. As in the example, the function
+## first analyze if getinverse is null, using the cache data or if not, 
+## calculate it with x$get
 
 cacheSolve <- function(x, ...) {
-      ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            ## message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
+
+## Example
+## We create a 2x2 matrix in variable temp
+## temp <- matrix(1:4, 2, 2)
+## temp
+## We calcule the inverse of the matrix
+## solve(temp)
+## temp2 <- makeCacheMatrix(temp)
+## The function obtain same value
+## cacheSolve(temp2)
+## cacheSolve(temp2)
+## Another one
+## temp3 <- matrix(runif(4000000), 2000, 2000)
+## temp4 <- makeCacheMatrix(temp3)
+## solve(temp3)
+## cacheSolve(temp4)
+## cacheSolve(temp4)
